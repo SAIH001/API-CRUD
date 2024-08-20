bb6265fb5aa8f766b922b9a9bb7be748eeaa4544
Syed Ali. Imran <aliimran@aptech.com>
2024-08-20 15:24:37 +0500

    createData function in javascript file

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-async function FetchUsers (){
+async function createDATA (){
+
+  // values contain from index html form 
+
+    var studentName = document.getElementById("studentName").value;
+    var studentEmail = document.getElementById("studentEmail").value;
+    var studentCourse = document.getElementById("studentCourse").value;
 
 
-    const userData = await fetch("https://66c07a67ba6f27ca9a56c433.mockapi.io/AptechNN/course");
+  // creating object for api where this object keys must be similar to api object keys  
+
+    const newUser = {
+        name: studentName,
+        email: studentEmail,
+        course: studentCourse,
+       
+      }
 
 
-    console.log(userData.json());
+
+    // const Response = await fetch("https://66c46cafb026f3cc6cef4c3e.mockapi.io/aptech/NewAddmission",{
+
+    // })
 
 
 }
-
-FetchUsers();
